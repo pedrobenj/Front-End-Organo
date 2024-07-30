649078f58efde3dd750916e5c8c9661f4b8e951c
Pedro Benjamin <benjamin23ph@gmail.com>
2024-07-30 14:44:46 -0300

    Projeto Finalizado

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ function App() {
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
+    debugger
     setColaboradores([...colaboradores, colaborador])
   }
 
